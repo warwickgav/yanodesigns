b4ae0db06da62cd0dd58b8f5f25e9368cfcaa11e
warwickgav <me@warwickgav.com>
2015-10-11 17:01:55 +1100

    Gallery
    Signed-off-by: warwickgav <me@warwickgav.com>

---
+++ site/templates/gallery.php
@@ -0,0 +1,20 @@
+<?php snippet('head') ?>
+<?php snippet('header') ?>
+
+<main class="site-content" role="main">
+        <div class="container">
+            <div class="page-header">
+                <h1 class="page-title"><?php echo $page->title()->html() ?></h1>
+            </div>
+            <div class="page-content">
+                <?php echo $page->text()->kirbytext() ?>
+	            <!-- SnapWidget -->
+<script src="http://snapwidget.com/js/snapwidget.js"></script>
+<iframe src="http://snapwidget.com/in/?u=eWFub19kZXNpZ25zfGlufDEyNXwzfDN8fG5vfDV8bm9uZXxvblN0YXJ0fG5vfHllcw==&ve=130915" title="Instagram Widget" class="snapwidget-widget" allowTransparency="true" frameborder="0" scrolling="no" style="border:none; overflow:hidden; width:100%;"></iframe>
+	        </div>
+        </div>
+  </main>
+<?php snippet('flair-services') ?>
+<?php snippet('footer') ?>
+
+
